de3cf2fb4ea56f5846e3a40c92aa93a66bebcfcf
Freddy Kwon <fredsckwon@gmail.com>
2021-11-18 19:12:30 -0500

    Modified README.md, index.html and myapp.js

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 A fully functional calculator program using HTML, CSS, Bootstrap and Javascript! 
 
 ## To Do
-Build HTML that structures a calculator with buttons (including a 'Clear' button) and a display, connect javascript functions to buttons. 
+Continue building functionality of calculator on js, build UI 
--- myapp.js
+++ myapp.js
@@ -1,21 +1,59 @@
 const add = (a, b) => a + b;
-
 const subtract = (a, b) => a - b;
-
 const multiply = (a, b) => a * b;
-
 const divide = (a, b) => a / b;
 
-const operate = (oper, a, b) => {
+const operate = (oper, inputNumber, b) => {
     let answer
     if (oper === "+") {
-        answer = add(a, b);
+        answer = add(inputNumber, b);
     } else if (oper === "-") {
-        answer = subtract(a, b);
+        answer = subtract(inputNumber, b);
     } else if (oper === "*") {
-        answer = multiply(a, b);
+        answer = multiply(inputNumber, b);
     } else if (oper === "/") {
-        answer = divide(a, b);
+        answer = divide(inputNumber, b);
     }
     return answer;
+}
+const display = document.querySelector("#display");
+const numButtons = document.querySelectorAll(".numBtns");
+const signButtons = document.querySelectorAll(".funcBtns")
+
+
+numButtons.forEach(btn => {
+    btn.addEventListener('click', function () {
+        inputNumber(this.id);
+    });
+});
+
+signButtons.forEach(btn => {
+    btn.addEventListener('click', function () {
+        signFunc(this.id);
+    });
+})
+
+let displayNumber;
+parseInt(displayNumber);
+
+// const inputNumber = number => {
+//     displayNumber = display.innerHTML;
+//     if (displayNumber.length < 16)
+//         display.innerHTML = number;
+//     return displayNumber;
+// }
+const inputNumber = number => {
+    displayNumber = display.innerHTML;
+    let n = 0;
+    while (displayNumber.length < 16) {
+        display.innerHTML = number;
+        return displayNumber;
+    }
+}
+
+
+const signFunc = sign => {
+    let displaySign = display.innerHTML;
+    display.innerHTML = sign;
+    return displaySign;
 }
