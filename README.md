7d858301db447bca1cc2b709dfa0c9d5aada9b4c
Freddy Kwon <fredsckwon@gmail.com>
2021-11-25 17:18:41 -0500

    Modified all files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Description 
-A fully functional calculator program using HTML, CSS, Bootstrap and Javascript! 
+A fully functional calculator program using HTML, CSS, Bulma and Javascript! 
 
 ## To Do
-Continue building functionality of calculator on js, build UI 
+Almost done! Still need to figure out percentage function, attaching keyboard clicks, refactoring the JS code and overall UI element. 
--- myapp.js
+++ myapp.js
@@ -1,59 +1,117 @@
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const calculator = document.querySelector("#calculator");
+const keys = calculator.querySelector(".calculatorKeys");
+const allClear = calculator.querySelector('[data-action=allClear');
+const clearEntry = calculator.querySelector('[data-action=clearEntry');
 
-const operate = (oper, inputNumber, b) => {
-    let answer
-    if (oper === "+") {
-        answer = add(inputNumber, b);
-    } else if (oper === "-") {
-        answer = subtract(inputNumber, b);
-    } else if (oper === "*") {
-        answer = multiply(inputNumber, b);
-    } else if (oper === "/") {
-        answer = divide(inputNumber, b);
+//add event listener for all buttons 
+keys.addEventListener("click", e => {
+    if (e.target.matches('button')) {
+        const displayNum = display.textContent;
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType
+
+        // function createResultString() {
+        // numbers
+        if (!action) {
+            if (displayNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayNum + keyContent;
+            }
+            calculator.dataset.previousKeyType = 'number';
+        }
+
+        //decimal 
+        if (action === 'decimal') {
+            if (!displayNum.includes('.')) {
+                display.textContent = displayNum + '.'
+            } else if (
+                previousKeyType === 'operator' ||
+                previousKeyType === 'calculate') {
+                display.textContent = '0.';
+            }
+            calculator.dataset.previousKeyType = 'decimal';
+        }
+
+        //operators 
+        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+            // button is depressed when clicking any action buttons
+            let firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayNum;
+            if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate') {
+                const calcValue = calculate(operator, firstValue, secondValue)
+                display.textContent = calcValue;
+                calculator.dataset.firstValue = calcValue;
+            } else {
+                calculator.dataset.firstValue = displayNum;
+            }
+            key.classList.add('is-depressed')
+            calculator.dataset.previousKeyType = 'operator';
+            calculator.dataset.operator = action;
+            //add custom attribute when operator button is clicked
+            Array.from(key.parentNode.children)
+                .forEach(k => k.classList.remove('is-depressed'))
+        }
+
+        //calculate or equal button
+        if (action === 'calculate') {
+            // calculator.dataset.previousKeyType = 'operator';
+            firstValue = calculator.dataset.firstValue;
+            operator = calculator.dataset.operator;
+            secondValue = displayNum;
+
+            if (firstValue) {
+                if (previousKeyType === 'calculate') {
+                    firstValue = displayNum;
+                    secondValue = calculator.dataset.modValue;
+                }
+                display.textContent = calculate(operator, firstValue, secondValue)
+            }
+            calculator.dataset.modValue = secondValue;
+            calculator.dataset.previousKeyType = 'calculate';
+        }
+
+
+        //clear buttons
+        if (action === 'allClear') {
+            if (key.textContent === 'AC') {
+                calculator.dataset.firstValue = '';
+                calculator.dataset.modValue = '';
+                calculator.dataset.operator = '';
+                calculator.dataset.previousKeyType = '';
+            }
+            display.textContent = 0;
+            calculator.dataset.previousKeyType = 'clear';
+        }
+        if (action === 'clearEntry') {
+            if (key.textContent === 'CE') {
+                calculator.dataset.previousKeyType = '';
+            }
+            display.textContent = 0;
+            calculator.dataset.previousKeyType = 'clear';
+        }
+    }
+    // }
+});
+
+
+//calculate function
+const calculate = (operator, a, b) => {
+    let answer = '';
+    if (operator === "add") {
+        answer = parseFloat(a) + parseFloat(b);
+    } else if (operator === 'subtract') {
+        answer = parseFloat(a) - parseFloat(b);
+    } else if (operator === "multiply") {
+        answer = parseFloat(a) * parseFloat(b);
+    } else if (operator === "divide") {
+        answer = parseFloat(a) / parseFloat(b);
     }
     return answer;
 }
-const display = document.querySelector("#display");
-const numButtons = document.querySelectorAll(".numBtns");
-const signButtons = document.querySelectorAll(".funcBtns")
 
-
-numButtons.forEach(btn => {
-    btn.addEventListener('click', function () {
-        inputNumber(this.id);
-    });
-});
-
-signButtons.forEach(btn => {
-    btn.addEventListener('click', function () {
-        signFunc(this.id);
-    });
-})
-
-let displayNumber;
-parseInt(displayNumber);
-
-// const inputNumber = number => {
-//     displayNumber = display.innerHTML;
-//     if (displayNumber.length < 16)
-//         display.innerHTML = number;
-//     return displayNumber;
-// }
-const inputNumber = number => {
-    displayNumber = display.innerHTML;
-    let n = 0;
-    while (displayNumber.length < 16) {
-        display.innerHTML = number;
-        return displayNumber;
-    }
-}
-
-
-const signFunc = sign => {
-    let displaySign = display.innerHTML;
-    display.innerHTML = sign;
-    return displaySign;
-}
+//to do 
+//add keyboard clicks to application
